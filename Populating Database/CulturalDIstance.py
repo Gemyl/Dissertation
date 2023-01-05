a4d5e6c5a104f8d4a3cacaa53e62dc5901b028db
Gemyl <115283416+Gemyl@users.noreply.github.com>
2023-01-05 18:43:31 +0200

    cultural_distances

--- Populating Database/CulturalDIstance.py
+++ Populating Database/CulturalDIstance.py
@@ -1,8 +1,11 @@
-from pybliometrics.scopus import AbstractRetrieval, AuthorRetrieval
-from geopy.geocoders import Nominatim
+from pybliometrics.scopus import AbstractRetrieval, AffiliationRetrieval
 from math import radians, sin, cos, sqrt, atan2
+from geopy.geocoders import Nominatim
+from DataRetrieving import get_DOIs
 from itertools import combinations
 from statistics import mean
+from tqdm import tqdm
+import pandas as pd
 
 def distance(lat1, lon1, lat2, lon2):
     # Convert latitude and longitude to radians
@@ -23,23 +26,72 @@ def distance(lat1, lon1, lat2, lon2):
     # Return the distance in kilometers
     return c * r
 
+minDist = []
+maxDist = []
+avgDist = []
+countries = []
 distances = []
 coordinates = []
+citationsCount = []
 affilCountries = []
+keywords = input('Keywords: ')
+yearsRange = input('Years range: ')
+subjects = input('Subjects: ').split(', ')
 
-doi = '10.1016/j.scs.2022.104089'
-authorsID = [author[0] for author in AbstractRetrieval(doi).authors]
-for author in authorsID:
-    affilCountries.append(AuthorRetrieval(author).affiliation_current[0][8])
+DOIs = get_DOIs(keywords,yearsRange, subjects)
 
-geolocator = Nominatim(user_agent = 'PersonalProject')
-for country in affilCountries:
-    location = geolocator.geocode(country)
-    coordinates.append((location.latitude, location.longitude))
+for doi in tqdm(DOIs):
+    citationsCount.append(AbstractRetrieval(doi).citedby_count)
+    authorsID = [author[0] for author in AbstractRetrieval(doi).authors]
+    for i in range(len(authorsID)):
+        try:
+            orgID = (AbstractRetrieval(doi).authors[i][4]).split(';')[0]
+            affilCountries.append(AffiliationRetrieval(orgID).country)
+        except:
+            continue
 
-locationsCombinations = list(combinations(coordinates, 2))
+    countries.append(affilCountries)
 
-for combo in locationsCombinations:
-    distances.append(distance(combo[0][0], combo[0][1], combo[1][0], combo[1][1]))
+    geolocator = Nominatim(user_agent = 'PersonalProject')
+    for country in affilCountries:
+        location = geolocator.geocode(country)
+        coordinates.append((location.latitude, location.longitude))
 
-print(f'Minimum Distance: {min(distances)} km, Maximum Distance: {max(distances)} km, Average Distance: {mean(distances)} km')
+    locationsCombinations = list(combinations(coordinates, 2))
+
+    for combo in locationsCombinations:
+        distances.append(distance(combo[0][0], combo[0][1], combo[1][0], combo[1][1]))
+    
+    try:
+        minDist.append(min(distances))
+    except:
+        minDist.append('-')
+
+    try:    
+        maxDist.append(max(distances))
+    except:
+        maxDist.append('-')
+
+    try:
+        avgDist.append(mean(distances))
+    except:
+        avgDist.append('-')
+    
+    distances = []
+    coordinates = []
+    affilCountries = []
+
+print(len(DOIs))
+print(len(citationsCount))
+print(len(minDist))
+print(len(maxDist))
+print(len(avgDist))
+
+cultDist = pd.DataFrame({'DOI':DOIs, 'Citations Count':citationsCount, 'Minimum Distance':minDist,
+    'Maximum Distance':maxDist, 'Average Distance':avgDist, 'Countries':countries})
+
+with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.precision', 3,
+                       ):
+    print(cultDist)
