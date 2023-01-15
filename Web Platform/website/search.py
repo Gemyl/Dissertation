dee810886ebe55804e35e113b97b79129bf02587
Gemyl <115283416+Gemyl@users.noreply.github.com>
2023-01-15 23:16:49 +0200

    fields_deletion

--- Web Platform/website/search.py
+++ Web Platform/website/search.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 SearchPage = Blueprint('SearchPage', __name__)
 
 @SearchPage.route('/search', methods = ['GET', 'POST'])
 def search():
+    data = request.form
+
+    for key in data.keys():
+        print(data[key])
     return render_template('search.html')
