ccd15e330c5146a22fa0b83d42baa6eacac54211
Gemyl <115283416+Gemyl@users.noreply.github.com>
2023-01-28 22:27:56 +0200

    google_api_search

--- Web Platform/insert2mysql/__init__.py
+++ Web Platform/insert2mysql/__init__.py
@@ -2,22 +2,23 @@ import mysql.connector as connector
 from tqdm import tqdm
 
 # connection to a MySQL database
+
+
 def connect_to_MySQL(password):
 
-    con = connector.connect(host = 'localhost',
-                                        port = '3306',
-                                        user = 'root',
-                                        password = password,
-                                        database = 'george',
-                                        auth_plugin = 'mysql_native_password')
+    con = connector.connect(host='localhost',
+                            port='3306',
+                            user='root',
+                            password=password,
+                            database='george',
+                            auth_plugin='mysql_native_password')
 
     cursor = con.cursor()
 
-    
-    return con, cursor, connector
+    return con, cursor
 
 
-# inserting publications data 
+# inserting publications data
 def insert_publications(cursor, doi, year, journal, authorsKeywords, userKeywords, subjects, title, citationsCount):
 
     for i in tqdm(range(len(doi))):
@@ -28,10 +29,13 @@ def insert_publications(cursor, doi, year, journal, authorsKeywords, userKeyword
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
 # inserting authors data
+
+
 def insert_authors(cursor, id, firstName, lastName, subjectedAreas, hIndex, itemCitations, authorsCitations, documentsCount):
 
     for i in tqdm(range(len(id))):
@@ -42,7 +46,8 @@ def insert_authors(cursor, id, firstName, lastName, subjectedAreas, hIndex, item
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
 
@@ -54,24 +59,29 @@ def insert_organizations(cursor, id, name, type, address, postalCode, city, stat
             query = 'INSERT INTO organizations VALUES (\'' + id[i] + '\', \'' + name[i] + '\', \'' + type[i] + '\', \'' + \
                 address[i] + '\', \'' + postalCode[i] + '\', \'' + city[i] + '\', \'' + state[i] + '\', \'' + \
                 country[i] + '\');'
-            
+
             cursor.execute(query)
 
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
 # insertion of publications and authors identifiers in a relational table
+
+
 def insert_publications_and_authors(cursor, doi, authorID):
 
     for i in tqdm(range(len(doi))):
-        query = 'INSERT INTO publications_authors (DOI, Author_ID) VALUES (\'' + doi[i] + '\', \'' + authorID[i] + '\');'
+        query = 'INSERT INTO publications_authors (DOI, Author_ID) VALUES (\'' + \
+            doi[i] + '\', \'' + authorID[i] + '\');'
         try:
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
 
@@ -80,24 +90,27 @@ def insert_publications_and_organizations(cursor, doi, orgID):
 
     for i in tqdm(range(len(doi))):
         query = 'INSERT INTO publications_organizations (DOI, Organization_ID) VALUES (\'' + doi[i] + '\', \'' + \
-        orgID[i] + '\');'
+            orgID[i] + '\');'
         try:
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
+
 
 def insert_authors_and_organizations(cursor, authorID, orgID):
 
     for i in tqdm(range(len(authorID))):
         query = 'INSERT INTO authors_organizations (Author_ID, Organization_ID) VALUES (\'' + \
-        authorID[i] + '\', \'' + orgID[i] + '\');'
+            authorID[i] + '\', \'' + orgID[i] + '\');'
         try:
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
 
@@ -110,10 +123,11 @@ def instert_cultural_distances(cursor, doi, citationsCount, minDist, maxDist, av
             cursor.execute(query)
         except connector.Error as err:
             if err.errno != 1062:
-                print(f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
+                print(
+                    f'Query Failed: {query}| Error code {err.errno}: {err.msg}')
             continue
 
-        
+
 # commitment of changes and termination of connection
 def commit_and_close(connection, cursor):
 
