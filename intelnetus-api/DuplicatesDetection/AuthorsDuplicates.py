2579db8ca7b09706a1186b7018c5d423a11f31f0
Gemyl <115283416+Gemyl@users.noreply.github.com>
2023-08-04 14:51:26 +0300

    202308041450 - Remove duplicates printing

--- intelnetus-api/DuplicatesDetection/AuthorsDuplicates.py
+++ intelnetus-api/DuplicatesDetection/AuthorsDuplicates.py
@@ -1,15 +1,4 @@
-from pybliometrics.scopus import AffiliationRetrieval
-import mysql.connector as connector
 from fuzzywuzzy import fuzz
-from getpass import getpass
-
-# Colors
-RED = "\033[1;31m"
-GREEN = "\033[1;32m"
-BLUE = "\033[1;34m"
-
-# Reset color to default
-RESET = "\033[0m"
 
 def detectAuthorsDuplicates(connection, cursor):
     # Lists for storing SQL data
@@ -24,8 +13,6 @@ def detectAuthorsDuplicates(connection, cursor):
     # Lists for concetrating removed and remaining variants
     variants1Ids = []
     variants2Ids = []
-    removedNames = []
-    remainingNames = []
 
     # Retrieving data from database
     query = 'SELECT ID FROM scopus_authors ORDER BY Last_Name;'
@@ -73,29 +60,19 @@ def detectAuthorsDuplicates(connection, cursor):
                 if (citationsCount[i] > citationsCount[j]):
                     variants1Ids.append(ids[i])
                     variants2Ids.append(ids[j])
-                    removedNames.append(f"{firstNames[j]} {lastNames[j]}")
-                    remainingNames.append(f"{firstNames[i]} {lastNames[i]}")
 
                 else:
                     variants1Ids.append(ids[j])
                     variants2Ids.append(ids[i])
-                    removedNames.append(f"{firstNames[i]} {lastNames[i]}")
-                    remainingNames.append(f"{firstNames[j]} {lastNames[j]}")
                       
             else:
                 break
 
-    if (len(removedNames) == 0):
-        print(f"{BLUE}No duplicates detected for fetched authors. {RESET}")
-    else:
-        for i in range(len(removedNames)):
-            print(f'------------------\n'
-                f'{GREEN}Remained variant: {remainingNames[i]}{RESET}\n'
-                f'{RED}Rejected variant: {removedNames[i]}{RESET}\n'
-                f'------------------')
-            try:
-                query = f"INSERT INTO scopus_authors_variants VALUES ('{variants1Ids[i]}', '{variants2Ids[i]}');"
-                cursor.execute(query)
-                connection.commit()
-            except:
-                pass
+    for i in range(len(variants1Ids)):
+        try:
+            query = f"INSERT INTO scopus_authors_variants VALUES ('{variants1Ids[i]}', '{variants2Ids[i]}');"
+            cursor.execute(query)
+            connection.commit()
+
+        except:
+            pass
